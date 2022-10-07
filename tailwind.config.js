84a3b251f46a688438c7aeb01ae63ee812650a75
nfal001 <111720828+nfal001@users.noreply.github.com>
2022-10-07 13:11:56 +0700

    First Deploy
    my first deploy

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0D4CBF",
+        secondary: "#313131",
+        grayish: "#838383",
+        lightblue: "#EFF7FF",
+        grayish2: "#C1C1C1",
+        lightblue2: "#6EAEF5",
+        myred: "#CA3E47",
+      },
+    },
+  },
+  plugins: [],
+};
